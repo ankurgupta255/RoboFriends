686c5a8601403484be19cb4debf92f5c9c556ee4
ankurgupta255 <43845288+ankurgupta255@users.noreply.github.com>
2019-03-28 20:11:16 +0530

    Updates

---
+++ precache-manifest.4e905f709d248b7b0fa1a2960be57ad0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6a05eb6cb7d034d3ccb0",
+    "url": "/RoboFriends/static/js/runtime~main.6a05eb6c.js"
+  },
+  {
+    "revision": "aa831a146f9a7642e140",
+    "url": "/RoboFriends/static/js/main.aa831a14.chunk.js"
+  },
+  {
+    "revision": "abb47184c0518133a477",
+    "url": "/RoboFriends/static/js/2.abb47184.chunk.js"
+  },
+  {
+    "revision": "aa831a146f9a7642e140",
+    "url": "/RoboFriends/static/css/main.2890b22e.chunk.css"
+  },
+  {
+    "revision": "abb47184c0518133a477",
+    "url": "/RoboFriends/static/css/2.921f1116.chunk.css"
+  },
+  {
+    "revision": "1d2beaf13fcf0613723ffd7e45c3fe9b",
+    "url": "/RoboFriends/index.html"
+  }
+];
